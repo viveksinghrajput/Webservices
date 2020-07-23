9a8ce910ebca509b7e3867a9d1d546c5011c16d1
v.bk.kumar <v.bk.kumar@BDC6-LX-02276Z9.dir.svc.accenture.com>
2020-07-23 17:14:12 +0530

    download in excel pdf json as well as csv formate

--- SpringFiles/src/main/java/com/accenture/controller/SpringFileReadController.java
+++ SpringFiles/src/main/java/com/accenture/controller/SpringFileReadController.java
@@ -1,7 +1,14 @@
 package com.accenture.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,6 +28,8 @@ public class SpringFileReadController {
 
 	@Autowired
 	private SpringFileReadService fileReadService;
+	@Autowired
+	private ServletContext context;
 
 	@GetMapping(value = "/")
 	public String Home(Model model) {
@@ -42,8 +52,9 @@ public class SpringFileReadController {
 	 */
 
 	@PostMapping("/import")
-	public String mapReapExcelDatatoDB(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) throws IOException {
-		boolean isFalg =fileReadService.saveDataFromFileUpload(file);
+	public String mapReapExcelDatatoDB(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+			throws IOException {
+		boolean isFalg = fileReadService.saveDataFromFileUpload(file);
 		if (isFalg) {
 			redirectAttributes.addFlashAttribute("successmessage", "File Upload SuccessFully!");
 		} else {
@@ -51,5 +62,76 @@ public class SpringFileReadController {
 		}
 		return "redirect:/";
 	}
+
+	@GetMapping("/downloadPdf")
+	public void exportPdfData(HttpServletRequest request, HttpServletResponse response) {
+
+		List<User> listuser = fileReadService.findAll();
+		boolean isFlag = fileReadService.createPdf(listuser, context, request, response);
+		if (isFlag) {
+			String fullPath = request.getServletContext().getRealPath("/resources/reports/" + "user" + ".pdf");
+			filedownload(fullPath, response, "user.pdf");
+		}
+
+	}
+	@GetMapping("/downloadExcel")
+	public void exportExcelData(HttpServletResponse response,HttpServletRequest request) {
+		List<User> listuser=fileReadService.findAll();
+		boolean isFlag=fileReadService.createExcel(listuser,context,request,response);
+		if(isFlag) {
+			String fullPath = context.getRealPath("/resources/reports/" + "user" + ".xls");//request.getServletContext().getRealPath("/resources/reports/" + "user" + ".xls");
+			filedownload(fullPath,response,"user.xls");
+		}
+	}
 	
+	@GetMapping("/downloadJson")
+	public void exportJsonData(HttpServletRequest request,HttpServletResponse response) {
+		List<User> listuser=fileReadService.findAll();
+		boolean isFlag=fileReadService.createJson(listuser,context);
+		if(isFlag) {
+			String fullPath = context.getRealPath("/resources/reports/" + "user.json");
+			filedownload(fullPath,response,"user.json");
+			
+		}
+		
+	}
+	@GetMapping("/downloadCsv")
+	public void exportCsvData(HttpServletRequest request,HttpServletResponse response) {
+		List<User> listuser=fileReadService.findAll();
+		boolean isFlag=fileReadService.createCSV(listuser,context);
+		if(isFlag) {
+			String fullPath = context.getRealPath("/resources/reports/" + "user.csv");
+			filedownload(fullPath,response,"user.csv");
+			
+		}
+
+	}
+	
+
+	private void filedownload(String fullPath, HttpServletResponse response, String fileName) {
+		File file = new File(fullPath);
+		final int BUFFER_SIZE = 4096;
+		if (file.exists()) {
+			try {
+				FileInputStream inputStream = new FileInputStream(file);
+				String mineType = context.getMimeType(fullPath);
+				response.setContentType(mineType);
+				response.setHeader("content-disposition", "attachment;filename=" + fileName);
+				OutputStream outputStream = response.getOutputStream();
+				byte[] buffer = new byte[BUFFER_SIZE];
+				int byteRead = -1;
+				while ((byteRead = inputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, byteRead);
+				}
+				inputStream.close();
+				outputStream.close();
+				file.delete();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 }
--- SpringFiles/src/main/java/com/accenture/service/SpringFileReadServiceImpl.java
+++ SpringFiles/src/main/java/com/accenture/service/SpringFileReadServiceImpl.java
@@ -1,19 +1,34 @@
 package com.accenture.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +37,19 @@ import org.springframework.web.multipart.MultipartFile;
 import com.accenture.model.User;
 import com.accenture.repository.SpringFileReadRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
 
 @Service
 @Transactional
@@ -104,7 +130,6 @@ public class SpringFileReadServiceImpl implements SpringFileReadService {
 	private boolean readDataFromCsv(MultipartFile file) {
 		try {
 			InputStreamReader inputStreamReader = new InputStreamReader(file.getInputStream());
-			// FileReader filereader = new FileReader(file);
 			CSVReader csvReader = new CSVReaderBuilder(inputStreamReader).withSkipLines(1).build();
 			List<String[]> allData = csvReader.readAll();
 			for (String[] row : allData) {
@@ -134,5 +159,245 @@ public class SpringFileReadServiceImpl implements SpringFileReadService {
 		}
 	}
 
-	
+	@Override
+	public boolean createPdf(List<User> listuser, ServletContext context, HttpServletRequest request,
+			HttpServletResponse response) {
+		Document document = new Document(PageSize.A4, 15, 15, 45, 30);
+		try {
+			String filePath = context.getRealPath("/resources/reports");
+			File file = new File(filePath);
+			boolean exist = new File(filePath).exists();
+			if (!exist) {
+				new File(filePath).mkdirs();
+			}
+
+			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file + "/" + "user" + ".pdf"));
+			document.open();
+
+			Font mainFont = FontFactory.getFont("Arial", 10, BaseColor.BLACK);
+
+			Paragraph paragraph = new Paragraph("All User", mainFont);
+			paragraph.setAlignment(Element.ALIGN_CENTER);
+			paragraph.setIndentationLeft(50);
+			paragraph.setIndentationRight(50);
+			paragraph.setSpacingAfter(10);
+			document.add(paragraph);
+
+			PdfPTable table = new PdfPTable(4);
+			table.setTotalWidth(100);
+			table.setSpacingAfter(10f);
+			table.setSpacingBefore(10f);
+
+			Font tableHeader = FontFactory.getFont("Arial", 10, BaseColor.BLACK);
+			Font tableBody = FontFactory.getFont("Arial", 9, BaseColor.BLACK);
+
+			float[] columnwidth = { 2f, 2f, 2f, 2f };
+			table.setWidths(columnwidth);
+
+			PdfPCell firstName = new PdfPCell(new Paragraph("First Name", tableHeader));
+			firstName.setBorderColor(BaseColor.BLACK);
+			firstName.setPaddingLeft(10f);
+			firstName.setHorizontalAlignment(Element.ALIGN_CENTER);
+			firstName.setVerticalAlignment(Element.ALIGN_CENTER);
+			firstName.setBackgroundColor(BaseColor.GRAY);
+			firstName.setExtraParagraphSpace(5f);
+			table.addCell(firstName);
+
+			PdfPCell lastName = new PdfPCell(new Paragraph("Last Name", tableHeader));
+			lastName.setBorderColor(BaseColor.BLACK);
+			lastName.setPaddingLeft(10f);
+			lastName.setHorizontalAlignment(Element.ALIGN_CENTER);
+			lastName.setVerticalAlignment(Element.ALIGN_CENTER);
+			lastName.setBackgroundColor(BaseColor.GRAY);
+			lastName.setExtraParagraphSpace(5f);
+			table.addCell(lastName);
+
+			PdfPCell email = new PdfPCell(new Paragraph("Email", tableHeader));
+			email.setBorderColor(BaseColor.BLACK);
+			email.setPaddingLeft(10f);
+			email.setHorizontalAlignment(Element.ALIGN_CENTER);
+			email.setVerticalAlignment(Element.ALIGN_CENTER);
+			email.setBackgroundColor(BaseColor.GRAY);
+			email.setExtraParagraphSpace(5f);
+			table.addCell(email);
+
+			PdfPCell phoneNumber = new PdfPCell(new Paragraph("Phone Number", tableHeader));
+			phoneNumber.setBorderColor(BaseColor.BLACK);
+			phoneNumber.setPaddingLeft(10f);
+			phoneNumber.setHorizontalAlignment(Element.ALIGN_CENTER);
+			phoneNumber.setVerticalAlignment(Element.ALIGN_CENTER);
+			phoneNumber.setBackgroundColor(BaseColor.GRAY);
+			phoneNumber.setExtraParagraphSpace(5f);
+			table.addCell(phoneNumber);
+
+			for (User user : listuser) {
+
+				PdfPCell firstNameValue = new PdfPCell(new Paragraph(user.getFirstName(), tableBody));
+				firstNameValue.setBorderColor(BaseColor.BLACK);
+				firstNameValue.setPaddingLeft(10f);
+				firstNameValue.setHorizontalAlignment(Element.ALIGN_CENTER);
+				firstNameValue.setVerticalAlignment(Element.ALIGN_CENTER);
+				firstNameValue.setBackgroundColor(BaseColor.WHITE);
+				firstNameValue.setExtraParagraphSpace(5f);
+				table.addCell(firstNameValue);
+
+				PdfPCell lastNamevalue = new PdfPCell(new Paragraph(user.getLastName(), tableBody));
+				lastNamevalue.setBorderColor(BaseColor.BLACK);
+				lastNamevalue.setPaddingLeft(10f);
+				lastNamevalue.setHorizontalAlignment(Element.ALIGN_CENTER);
+				lastNamevalue.setVerticalAlignment(Element.ALIGN_CENTER);
+				lastNamevalue.setBackgroundColor(BaseColor.WHITE);
+				lastNamevalue.setExtraParagraphSpace(5f);
+				table.addCell(lastNamevalue);
+
+				PdfPCell emailvalue = new PdfPCell(new Paragraph(user.getEmail(), tableBody));
+				emailvalue.setBorderColor(BaseColor.BLACK);
+				emailvalue.setPaddingLeft(10f);
+				emailvalue.setHorizontalAlignment(Element.ALIGN_CENTER);
+				emailvalue.setVerticalAlignment(Element.ALIGN_CENTER);
+				emailvalue.setBackgroundColor(BaseColor.WHITE);
+				emailvalue.setExtraParagraphSpace(5f);
+				table.addCell(emailvalue);
+
+				PdfPCell phoneNumbervalue = new PdfPCell(new Paragraph(user.getPhoneNumber(), tableBody));
+				phoneNumbervalue.setBorderColor(BaseColor.BLACK);
+				phoneNumbervalue.setPaddingLeft(10f);
+				phoneNumbervalue.setHorizontalAlignment(Element.ALIGN_CENTER);
+				phoneNumbervalue.setVerticalAlignment(Element.ALIGN_CENTER);
+				phoneNumbervalue.setBackgroundColor(BaseColor.WHITE);
+				phoneNumbervalue.setExtraParagraphSpace(5f);
+				table.addCell(phoneNumbervalue);
+			}
+			document.add(table);
+			document.close();
+			writer.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean createExcel(List<User> listuser, ServletContext context, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		String filePath = context.getRealPath("/resources/reports");
+		File file = new File(filePath);
+		boolean exist = new File(filePath).exists();
+		if (!exist) {
+			new File(filePath).mkdirs();
+		}
+		try {
+
+			FileOutputStream OutputStream = new FileOutputStream(file + "/" + "user" + ".xls");
+			HSSFWorkbook workBook = new HSSFWorkbook();
+
+			HSSFSheet worksheet = workBook.createSheet("user");
+			worksheet.setDefaultColumnWidth(30);
+
+			HSSFCellStyle headercellStyle = workBook.createCellStyle();
+			headercellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_50_PERCENT.getIndex());
+			headercellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+			HSSFRow headrow = worksheet.createRow(0);
+			// Header decoration
+			HSSFCell Firstname = headrow.createCell(0);
+			Firstname.setCellValue("First Name");
+			Firstname.setCellStyle(headercellStyle);
+
+			HSSFCell Lastname = headrow.createCell(1);
+			Lastname.setCellValue("Last Name");
+			Lastname.setCellStyle(headercellStyle);
+
+			HSSFCell email = headrow.createCell(2);
+			email.setCellValue("Email");
+			email.setCellStyle(headercellStyle);
+
+			HSSFCell phoneNumber = headrow.createCell(3);
+			phoneNumber.setCellValue("Phone Number");
+			phoneNumber.setCellStyle(headercellStyle);
+
+			int i = 1;
+			for (User user : listuser) {
+				HSSFRow bodyRow = worksheet.createRow(i);
+
+				HSSFCellStyle bodyCellStyle = workBook.createCellStyle();
+				bodyCellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.WHITE.getIndex());
+
+				HSSFCell FirstnameValue = bodyRow.createCell(0);
+				FirstnameValue.setCellValue(user.getFirstName());
+				FirstnameValue.setCellStyle(bodyCellStyle);
+
+				HSSFCell LastnameValue = bodyRow.createCell(1);
+				LastnameValue.setCellValue(user.getLastName());
+				LastnameValue.setCellStyle(bodyCellStyle);
+
+				HSSFCell emailValue = bodyRow.createCell(2);
+				emailValue.setCellValue(user.getEmail());
+				emailValue.setCellStyle(bodyCellStyle);
+
+				HSSFCell phoneNumberValue = bodyRow.createCell(3);
+				phoneNumberValue.setCellValue(user.getPhoneNumber());
+				phoneNumberValue.setCellStyle(bodyCellStyle);
+
+				i++;
+			}
+			workBook.write(OutputStream);
+			OutputStream.flush();
+			OutputStream.close();
+			return true;
+
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean createJson(List<User> listuser, ServletContext context) {
+
+		String filePath = context.getRealPath("/resources/reports");
+		boolean exist = new File(filePath).exists();
+		if (!exist) {
+			new File(filePath).mkdirs();
+		}
+
+		File file = new File(filePath + "/" + File.separator + "user.json");
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(file, listuser);
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean createCSV(List<User> listuser, ServletContext context) {
+		String filePath = context.getRealPath("/resources/reports");
+		boolean exist = new File(filePath).exists();
+		if (!exist) {
+			new File(filePath).mkdirs();
+		}
+
+		File file = new File(filePath + "/" + File.separator + "user.csv");
+		try {
+			FileWriter fileWriter=new FileWriter(file);
+			CSVWriter csvWriter=new CSVWriter(fileWriter);
+			List<String[]> data=new ArrayList<>();
+			data.add(new String[] {"First Name","Last Name","Email","Phone Number"});
+			for(User user: listuser) {
+				data.add(new String[] {user.getFirstName(),user.getLastName(),user.getEmail(),user.getPhoneNumber()});
+			}
+			csvWriter.writeAll(data);
+			csvWriter.close();
+			return true;
+		}catch (Exception e) {
+			return false;
+		}
+		
+		
+		
+	}
+
 }
